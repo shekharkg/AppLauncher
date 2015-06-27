50adc3a93684fbee2317bf029e7da52088247749
ShekharKG <shekhar114@gmail.com>
2015-06-28 02:59:10 +0530

    App drag drop added

---
+++ app/src/main/java/com/shekhar/launcher/ApplicationViewer.java
@@ -0,0 +1,56 @@
+package com.shekhar.launcher;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.GridView;
+import android.widget.RelativeLayout;
+
+import com.shekhar.launcher.adapter.ApplicationAdapter;
+import com.shekhar.launcher.dao.ApplicationDetailsModel;
+import com.shekhar.launcher.database.ApplicationDataBase;
+
+import java.util.List;
+
+/**
+ * Created by Mradul on 6/27/2015.
+ */
+public class ApplicationViewer extends Activity {
+
+  public static List<ApplicationDetailsModel> applicationDetailsModels;
+  private String intentData;
+
+  @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.app_viewer);
+
+    applicationDetailsModels = ApplicationDataBase.getSingletonInstance(this).getAllApplications();
+
+    Log.e("size", String.valueOf(applicationDetailsModels.size()));
+    if (getActionBar() != null)
+      getActionBar().hide();
+    RelativeLayout appViewer = (RelativeLayout) findViewById(R.id.appViewer);
+    appViewer.setBackground(getResources().getDrawable(R.drawable.bg));
+    intentData = getIntent().getStringExtra("ID");
+    if (intentData != null) {
+      AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+      dialog.setMessage("Select any one application for replacement");
+      dialog.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialogInterface, int i) {
+
+        }
+      });
+      dialog.show();
+    }
+    GridView gridView = (GridView) findViewById(android.R.id.list);
+    gridView.setAdapter(new ApplicationAdapter(applicationDetailsModels, intentData, this));
+  }
+}
--- app/src/main/java/com/shekhar/launcher/MainActivity.java
+++ app/src/main/java/com/shekhar/launcher/MainActivity.java
@@ -1,23 +1,25 @@
 package com.shekhar.launcher;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
+import android.content.ClipData;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Point;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.DragEvent;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.Toast;
+import android.widget.ImageView;
 
+import com.github.siyamed.shapeimageview.OctogonImageView;
 import com.shekhar.launcher.async.AddApplicationDetailsAsync;
 import com.shekhar.launcher.dao.ApplicationDetailsModel;
 import com.shekhar.launcher.database.ApplicationDataBase;
 import com.shekhar.launcher.intrface.CallBack;
 
-import java.util.Calendar;
 import java.util.List;
 
 
@@ -26,6 +28,11 @@ public class MainActivity extends Activity implements CallBack {
   private List<ApplicationDetailsModel> applicationDetailsModels;
   private final String TAG = "<<MainActivity>>";
   ApplicationDataBase applicationDataBase;
+
+  private ImageView replace;
+  public OctogonImageView[] img;
+  private String selectedId;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -37,34 +44,118 @@ public class MainActivity extends Activity implements CallBack {
     if (applicationDetailsModels.size() < 1)
       new AddApplicationDetailsAsync(this, this).execute();
     else
-      showList();
+      loadAppsInHomeScreen();
+
   }
 
-  private void showList() {
-    ListView listView = (ListView) findViewById(R.id.listView);
-    String[] appNames = new String[applicationDetailsModels.size()];
-    for (int index = 0; index < applicationDetailsModels.size(); index++) {
-      appNames[index] = applicationDetailsModels.get(index).getAppName();
+  private void loadApplicationImageView() {
+    for (int i = 0; i < 8; i++) {
+      final ApplicationDetailsModel model = applicationDetailsModels.get(i);
+      img[i].setOnLongClickListener(new View.OnLongClickListener() {
+        @Override
+        public boolean onLongClick(View view) {
+          replace.setVisibility(View.VISIBLE);
+          selectedId = String.valueOf(model.getId());
+          Dragshadow shadowBuilder = new Dragshadow(view);
+          ClipData data = ClipData.newPlainText(" ", " ");
+          view.startDrag(data, shadowBuilder, view, 0);
+          return false;
+        }
+      });
+
+      img[i].setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+          run(model.getAppPackage());
+        }
+      });
+
+      try {
+        img[i].setImageDrawable(getPackageManager().getApplicationIcon(model.getAppPackage()));
+      } catch (PackageManager.NameNotFoundException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+
+  public void run(String packageName) {
+    Log.e("run", packageName, null);
+    try {
+      Intent intent = this.getPackageManager().getLaunchIntentForPackage(packageName);
+      this.startActivity(intent);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private class Dragshadow extends View.DragShadowBuilder {
+    ColorDrawable grayShedow;
+
+    public Dragshadow(View view) {
+      super(view);
+      grayShedow = new ColorDrawable(Color.LTGRAY);
     }
 
-//    listView.setAdapter(new ArrayAdapter<String>(this,
-//        android.R.layout.simple_list_item_1, appNames));
-//
-//    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//      @Override
-//      public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//        ApplicationDetailsModel model = applicationDetailsModels.get(position);
-//        Calendar calendar = Calendar.getInstance();
-//        int frequencyCount = model.getFrequencyCount();
-//        applicationDataBase.updateAppDetails(model.getId(),
-//            calendar.getTimeInMillis(), ++frequencyCount);
-//
-//        model.setFrequencyCount(frequencyCount);
-//        model.setLastUsedOn(calendar.getTimeInMillis());
-//
-//        Toast.makeText(MainActivity.this, model.getFrequencyCount() + "", Toast.LENGTH_SHORT).show();
-//      }
-//    });
+    @Override
+    public void onDrawShadow(Canvas canvas) {
+      grayShedow.draw(canvas);
+    }
+
+    @Override
+    public void onProvideShadowMetrics(Point shadowSize, Point shadowTouchPoint) {
+      View v = getView();
+      int h = v.getHeight();
+      int w = v.getWidth();
+      grayShedow.setBounds(0, 0, h, w);
+      shadowSize.set(h, w);
+      shadowTouchPoint.set(h, w);
+    }
+
+  }
+
+  View.OnDragListener dragListener = new View.OnDragListener() {
+    @Override
+    public boolean onDrag(View view, DragEvent dragEvent) {
+      int event = dragEvent.getAction();
+      switch (event) {
+        case DragEvent.ACTION_DRAG_ENTERED:
+          Log.e("event", "entered");
+          break;
+        case DragEvent.ACTION_DRAG_EXITED:
+          Log.e("event", "exit");
+          break;
+        case DragEvent.ACTION_DROP:
+          ImageView targetView = (ImageView) view;
+          if (targetView.getId() == replace.getId()) {
+            Intent intent = new Intent(MainActivity.this, ApplicationViewer.class);
+            intent.putExtra("ID", selectedId);
+            MainActivity.this.startActivity(intent);
+          }
+          break;
+        case DragEvent.ACTION_DRAG_ENDED:
+          Log.e("event", "ended");
+          replace.setVisibility(View.GONE);
+          break;
+      }
+      return true;
+    }
+  };
+
+
+  private void loadAppsInHomeScreen() {
+    img = new OctogonImageView[8];
+    replace = (ImageView) findViewById(R.id.replace);
+    img[0] = (OctogonImageView) findViewById(R.id.image1);
+    img[1] = (OctogonImageView) findViewById(R.id.image2);
+    img[2] = (OctogonImageView) findViewById(R.id.image3);
+    img[3] = (OctogonImageView) findViewById(R.id.image4);
+    img[4] = (OctogonImageView) findViewById(R.id.image5);
+    img[5] = (OctogonImageView) findViewById(R.id.image6);
+    img[6] = (OctogonImageView) findViewById(R.id.image7);
+    img[7] = (OctogonImageView) findViewById(R.id.image8);
+    replace.setOnDragListener(dragListener);
+
+    loadApplicationImageView();
   }
 
   @Override
@@ -79,9 +170,8 @@ public class MainActivity extends Activity implements CallBack {
   public void asyncTaskCompleteCallBack() {
     applicationDetailsModels = ApplicationDataBase.getSingletonInstance(this).getAllApplications();
     if (applicationDetailsModels.size() > 0)
-      showList();
+      loadAppsInHomeScreen();
 
   }
-
 
 }
